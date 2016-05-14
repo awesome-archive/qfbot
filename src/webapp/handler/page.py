95554f7fefe9fe649a12604813a75e7eb96d33f7
addbook <oskcloud@gmail.com>
2016-05-14 15:21:42 +0800

    ajax.login

--- src/webapp/handler/page.py
+++ src/webapp/handler/page.py
@@ -5,12 +5,51 @@
 # Created: 2016-05-07 09:30 CST
 
 
-from .base import BaseHandler, ApiAuthBaseHandler
-from .. import setting
-
-
 from tornado import gen
 from tornado import web
+import logging
+import json
+
+from .base import BaseHandler, ApiAuthBaseHandler
+from ..tools.authenticate import Authenticate
+# from .. import setting
+
+
+class IndexHandler(ApiAuthBaseHandler):
+
+    @gen.engine
+    def _authenticate(self, callback=None):
+        token = self.get_secure_cookie("Token")
+        if not token:
+            self.redirect("/login")
+            return
+        user_id = yield gen.Task(self.rdc.get, token)
+        user = yield gen.Task(self.db["user"].find_one, {"_id": user_id})
+        if user:
+            self.current_user = user_id
+        else:
+            self.redirect("/login")
+            return
+        if hasattr(self, "_role"):
+            if user.get("role", 0) < self._role:
+                self.redirect("/login")
+                return
+        if callback:
+            callback(user)
+
+    @web.asynchronous
+    @gen.coroutine
+    def _get_(self):
+        user = yield gen.Task(self._authenticate)
+        if not self.current_user:
+            self.redirect("/login")
+            return
+        else:
+            values = {
+                "user": user
+            }
+            self.render("index.html", **values)
+
 
 class LoginHandler(BaseHandler):
 
@@ -46,7 +85,29 @@ class LoginHandler(BaseHandler):
     @gen.coroutine
     def post(self, *args, **kwargs):
         """
+            ajax api: LOGIN.POST
         """
+        try:
+            data = json.loads(self.request.body)
+        except Exception, e:
+            logging.error(e)
+            self.write({"code": 410, "detail": u"email or password error"})
+            self.finish()
+            return
+        email = data.get("email", "")
+        password = data.get("password")
+        find = yield self.db['user'].find_one({"email": email})
+        if not find:
+            self.write({"code": 410, "detail": u"email or password error"})
+            self.finish()
+            return
+        salt = find.get("salt")
+        hash_pw = find.get("hash_pw")
+        if Authenticate.check_password(salt, password, hash_pw):
+            self.set_secure_cookie("Token", find.get("user_id"))
+            self.write({"code": 200, "detail": "login success"})
+            self.finish()
+            return
 
 
 class SignupHandler(BaseHandler):
@@ -86,40 +147,29 @@ class PasswordResetHandler(ApiAuthBaseHandler):
         :return:
         """
 
-class IndexHandler(ApiAuthBaseHandler):
 
-    @gen.engine
-    def _authenticate(self, callback=None):
-        token = self.get_secure_cookie("Token")
-        if not token:
-            self.redirect("/login")
-            return
-        user_id = yield gen.Task(self.rdc.get, token)
-        user = yield self.db["user"].find_one({"_id": user_id})
-        if user:
-            self.current_user = user_id
-        else:
-            self.redirect("/login")
-            return
-        if hasattr(self, "_role"):
-            if user.get("role", 0) < self._role:
-                self.redirect("/")
-                return
-        if callback:
-            callback(user)
+class PasswordForgotHandler(BaseHandler):
+    """
+    API: 重置密码API
+    """
 
     @web.asynchronous
     @gen.coroutine
-    def _get_(self):
-        user = yield gen.Task(self._authenticate)
-        if not self.current_user:
-            self.redirect("/login")
-            return
-        else:
-            values = {
-                "user": user
-            }
-            self.render("index.html", **values)
+    def get(self, *args, **kwargs):
+        """
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
+    @web.asynchronous
+    @gen.coroutine
+    def post(self, *args, **kwargs):
+        """
+        :param args:
+        :param kwargs:
+        :return:
+        """
 
 
 page_routes = [
@@ -127,5 +177,6 @@ page_routes = [
     (r"/signup/?", SignupHandler),
     (r"/logout/?", LogoutHandler),
     (r"/password_reset/?", PasswordResetHandler),
+    (r"/password_forget/?", PasswordForgotHandler),
     (r'/?', IndexHandler)
 ]
--- src/webapp/tools/authenticate.py
+++ src/webapp/tools/authenticate.py
@@ -5,3 +5,41 @@
 # Created: 2016-05-04 22:29 CST
 
 
+import hashlib
+
+
+class Authenticate(object):
+    """
+    """
+
+    @classmethod
+    def check_password(cls, salt, raw_pass, hash_pass):
+        """
+        :param salt:
+        :param raw_pass:
+        :param hash_pass:
+        :return:
+        """
+        _hash_pass = cls.encrypt(salt, raw_pass)
+        if str(_hash_pass) == str(hash_pass):
+            return True
+        else:
+            return False
+
+    @classmethod
+    def user_view(cls, user):
+        """
+        :param user:
+        :return:
+        """
+
+    @staticmethod
+    def encrypt(salt, raw_pass):
+        """
+        :param salt:
+        :param raw_pass:
+        :return:
+        """
+        to_hash = salt.encode("utf-8") + raw_pass.encode("utf-8")
+        hash_pw = hashlib.sha256(to_hash).hexdigest()
+        return hash_pw
