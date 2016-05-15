92c258a6cb7bf7586fae89b4eafb78bd2d625dd2
addbook <oskcloud@gmail.com>
2016-05-15 22:42:31 +0800

    project and rawdata api

--- src/webapp/handler/project.py
+++ src/webapp/handler/project.py
@@ -5,3 +5,67 @@
 # Created: 2016-05-04 22:28 CST
 
 
+from bson import json_util
+from tornado.web import HTTPError
+import json
+import datetime
+import logging
+from tornado import gen
+from tornado import web
+
+# from ..model import doc
+from .base import ApiAuthBaseHandler
+
+
+class ProjectHandler(ApiAuthBaseHandler):
+
+    @web.asynchronous
+    @gen.coroutine
+    def _get_(self):
+        project_id = self.get_argument("project_id", None)
+        _type = self.get_argument("_type", None)
+        next_id = self.get_argument("next_id", None)
+        if project_id:
+            project = yield self.db["project"].find_one({"_id": project_id})
+            self.write_message({"code": 200, "detail": project})
+            return
+        else:
+            query = {}
+            if _type:
+                query.update({"_type": _type})
+            if next_id:
+                query.update({"_id": {"$lte": next_id}})
+            projects = yield self.db["project"].find(query)
+            count = projects.count()
+
+    @web.asynchronous
+    @gen.coroutine
+    def _post_(self):
+        """
+        :return:
+        """
+        try:
+            _dat = json.loads(self.request.body)
+        except ValueError, e:
+            logging.error(e)
+            self.write_message({"code": 400, "detail": "error data type"})
+            return
+
+    @web.asynchronous
+    @gen.coroutine
+    def _put_(self):
+        """
+        :return:
+        """
+
+    @web.asynchronous
+    @gen.coroutine
+    def _delete_(self):
+        """
+        :return:
+        """
+
+
+project_routes = [
+    (r'/project/?', ProjectHandler),
+]
--- src/webapp/handler/rawdata.py
+++ src/webapp/handler/rawdata.py
@@ -4,4 +4,151 @@
 # Author: YuanLin
 # Created: 2016-05-04 22:28 CST
 
+import datetime
+from tornado.gen import coroutine
+from tornado import gen
+from tornado.web import HTTPError
+import json
+from bson.code import Code
+from bson import json_util
 
+from .base import ApiAuthBaseHandler
+
+
+class DataHandler(ApiAuthBaseHandler):
+
+    @coroutine
+    def get_posts(self, category, status, page_num, project, qtime):
+        query = {"expired": None}
+
+        if status != "ALL":
+            if status == "fail":
+                status = 0
+            query.update({"status": status})
+        if category:
+            query.update({"category": category})
+        if project:
+            query.update({"source": project})
+        if qtime:
+            query.update(qtime)
+        cursor = (self.db["rawdata"].find(query)
+                  .skip(int(page_num)*10)
+                  .limit(10))
+
+        res = yield cursor.to_list(10)
+        cnts = yield self.db["rawdata"].find(query).count()
+        result = {"data": res, "total": cnts}
+        raise gen.Return(result)
+
+    @coroutine
+    def _get_(self):
+        status = self.get_argument("status", default=None)
+        category = self.get_argument("category", default=None)
+        page_num = self.get_argument("page", default=0)
+        tm = self.get_argument("time", default=None)
+        project = self.get_argument("project", default=None)
+
+        if status == 'all':
+            status_code = 'ALL'
+        elif status == "pass":
+            status_code = 1
+        elif status == "fail":
+            status_code = "fail"
+        else:
+            status_code = None
+        if category == 'null':
+            category = None
+        td = datetime.date.today()
+        dt = datetime.datetime(td.year, td.month, td.day)
+        if tm == "today":
+            dates = {"updated": {"$gte": dt}}
+        elif tm == "older":
+            dates = {"updated": {"$lt": dt}}
+        else:
+            dates = None
+
+        result = yield self.get_posts(category, status_code, page_num, project, dates)
+        if not (status_code or category or tm or project or dates):
+            cnts = yield self.group_prj()
+            result["total"] = cnts
+
+        message = json.dumps(result, default=json_util.default)
+        self.write(message)
+        self.finish()
+
+    @coroutine
+    def _post_(self):
+        data = json.loads(self.request.body)
+        if not data:
+            raise HTTPError(400)
+        message = {}
+        update_data = data.get('UPDATE')
+        status = data.get('STATUS')
+        if update_data:
+            message = yield self.bulk_update(update_data)
+
+        if status:
+            message = yield self.modify_status(status)
+
+        if not message:
+            raise HTTPError(400)
+        self.write(json.dumps(message, default=json_util.default))
+        self.finish()
+
+    @coroutine
+    def bulk_update(self, data):
+        if not isinstance(data, dict):
+            raise HTTPError(400)
+        p_id = data.pop("_id")
+        oid = self.validate_id(p_id)
+        if oid:
+            result = yield self.db['rawdata'].update({"_id":oid}, {'$set':data})
+            if result.get('updatedExisting'):
+                msg = {"code": 200, "msg": "update succeed"}
+                raise gen.Return(msg)
+            raise gen.Return({"code": 302, "detail": "update failed"})
+        else:
+            raise HTTPError(400)
+
+    @coroutine
+    def modify_status(self, status):
+        """Modify status
+        """
+        p_id = status.pop("_id")
+        oid = self.validate_id(p_id)
+        if status["status"] == 1:
+            result = yield self.db['rawdata'].find_one({"_id": oid})
+            user = self.current_user['email']
+            result.update({"email": user, "data-source": "crawl"})
+            json_str = json.dumps(result, default=json_util.default)
+            self.rdc.sadd("crawl:rawdata", json_str)
+
+        if oid:
+            result = yield self.db['rawdata'].update(
+                {"_id": oid}, {"$set": status})
+            if result.get('updatedExisting'):
+                msg = {"code": 200, "msg": "update succeed"}
+                raise gen.Return(msg)
+            print result
+
+            raise gen.Return({"code": 302, "detail": "update failed"})
+        else:
+            raise HTTPError(400)
+
+    @gen.coroutine
+    def group_prj(self):
+        reducer = Code(
+            """
+                function(obj, prev){
+                    prev.count++;
+                }
+            """
+        )
+        results = yield self.db['rawdata'].group(
+            key={"source"}, condition={'expired': None},
+            initial={"count": 0}, reduce=reducer)
+        r = {}
+        for doc in results:
+            d = doc.get('source')
+            r[d] = doc.get('count')
+        raise gen.Return(r)
