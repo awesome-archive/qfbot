2f621addd5222ff65bd508234893b94210d7297c
targc <targc@tesool.com>
2016-06-05 00:51:43 +0800

    update

--- run_webapp.sh
+++ run_webapp.sh
@@ -3,3 +3,7 @@
 cp Dockerfile_webapp Dockerfile
 docker build -t qfbot-webapp .
 
+docker ps -a |grep "qfbot-webapp"|awk '{print $1}'|xargs docker rm -f
+
+docker run -d --restart="always" --name="qfbot-webapp" -p $1:8888 qfbot-webapp
+
