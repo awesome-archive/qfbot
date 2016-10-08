7abc10756b965d9fce3ca561209453a63449da92
addbook <addbook@live.com>
2016-10-08 11:47:28 +0800

    pubish a spider

--- src/crawl_dispatcher.py
+++ src/crawl_dispatcher.py
@@ -6,19 +6,43 @@
 from tornado import ioloop
 from tornado import gen
 import pika
+# from stormed import Connection, Message
 import logging
-
+try:
+    import cPickle as pickle
+except:
+    import pickle
+from crawler.tools.db import conn
+from crawler.dispather import CrawlerFactory
+from webapp import setting
+# from crawler.spider.pagecrawler import PageDirectSpider
 
 def generate():
     """
     """
+    # projects = conn.project.find({})
+    urls = "http://book.douban.com"
+    projects = {}
+    spider = CrawlerFactory("RUN").create(projects)
+    publish(spider)
 
-def publish():
+
+def publish(obj):
     """
     """
+    params = pika.URLParameters(setting.mq_urls)
+    connection = pika.BlockingConnection(params)
+    channel.basic_publish('test_exchange',
+                      'test_routing_key',
+                      pickle.dumps(obj),
+                      pika.BasicProperties(content_type='text/plain',
+                                           delivery_mode=1))
+    connection.close()
 
 
 if __name__ == "__main__":
+    # conn = Connection(host="localhost")
+    # conn.connect(gern)
     ioloop.PeriodicCallback(generate, 3000).start()
     ioloop.IOLoop.instance().start()
 
